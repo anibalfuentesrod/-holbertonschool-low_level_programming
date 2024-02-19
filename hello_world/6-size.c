15e5a6b1d817dd2d8fa27d22c73bc28e8c70522b
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-02-19 15:55:49 -0400

    making the 6 with steven help

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,15 @@
 */
 int main(void)
 {
-printf("size of char: %lu byte(s)\n", sizeof(char));
-printf("size of int: %lu byte(s)\n", sizeof(int));
-printf("size of long: %lu byte(s)\n", sizeof(long int));
-printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
-printf("size of float: %lu byte(s)\n", sizeof(float));
+char = onebyte;
+int = fourbytes;
+long int = bytessix;
+long long int = eightbytes;
+float = forbytes;
+printf("size of char: %zu\n", sizeof(char));
+printf("size of int: %zu\n", sizeof(int));
+printf("size of long: %zu\n", sizeof(long int));
+printf("size of long long int: %zu\n", sizeof(long long int));
+printf("size of float: %zu\n", sizeof(float));
 return (0);
 }
