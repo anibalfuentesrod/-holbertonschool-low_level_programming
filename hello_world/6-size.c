d28f2a1f295c748bec68a02881a88792cdb57c4a
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-02-19 16:00:25 -0400

    deletinf something

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,15 +6,15 @@
 */
 int main(void)
 {
-char = onebyte;
-int = fourbytes;
-long int = bytessix;
-long long int = eightbytes;
-float = forbytes;
-printf("size of char: %zu\n", sizeof(char));
-printf("size of int: %zu\n", sizeof(int));
-printf("size of long: %zu\n", sizeof(long int));
-printf("size of long long int: %zu\n", sizeof(long long int));
-printf("size of float: %zu\n", sizeof(float));
+char
+int 
+long int 
+long long int 
+float 
+printf("size of char: %zu \n", sizeof(char));
+printf("size of int: %zu \n", sizeof(int));
+printf("size of long: %zu \n", sizeof(long int));
+printf("size of long long int: %zu \n", sizeof(long long int));
+printf("size of float: %zu \n", sizeof(float));
 return (0);
 }
