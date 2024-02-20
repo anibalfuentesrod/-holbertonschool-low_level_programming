1200fd48a87163d48b84cea75ab7ef0eb46af0d1
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-02-20 11:43:38 -0400

    adding %

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
- printf("is positive, %d \n");
+ printf("%d is positive", n);
 }
 else if (n == 0)
 {
-prinf("is zero", \n");
+printf("%d is zero", n);
 }
 else
 {
-printf("is negative, %d \n");
+printf("%d is negative", n);
 }
 return (0);
 }
