c0ff15833013d77ca13c47faa8c08588e7f8ee21
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-02-21 14:18:23 -0400

    making the 9 with just a little of help

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - making the nine
+ *
+ * Return: 0
+**/
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+{
+	putchar(num + '0');
+	putchar(',');
+	putchar(' ');
+}
+putchar('\n');
+return (0);
+}
