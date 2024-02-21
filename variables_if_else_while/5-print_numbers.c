228e1941b1d9439548ea2d3b7eb2916223ebcce0
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-02-21 10:22:31 -0400

    making the 5 and again

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - making the numbers
+ *
+ * Return: 0
+ *
+**/
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+{
+printf("%d", num);
+}
+printf("\n");
+return (0);
+}
