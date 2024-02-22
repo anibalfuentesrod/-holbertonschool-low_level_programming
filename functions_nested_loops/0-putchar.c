210a7e0133067874e6cf844535c776c01678e070
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-02-21 21:35:19 -0400

    alone putchar jaja

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -4,9 +4,18 @@
  *
  * Return: 0
 **/
+void print_until_newline(char *s);//ACEPTA EL STRING COMO UN ARGUMENT
+
 int main(void)
 {
-
-printf("_putchar\n");
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
 return (0);
 }
