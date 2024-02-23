e25adec4a6bdd53c43224e70b1fed00c80b6372d
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-02-23 18:37:41 -0400

    making the 7 with alot of help from chatgpt

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,10 @@ int print_last_digit(int number)
 {
 int last = number % 10;
 if (last < 0)
-	_putchar(last);
-return (0);
+	last *= -1;
+{
+	_putchar(last + '0');
+}
+_putchar('\n');
+return (last);
 }
