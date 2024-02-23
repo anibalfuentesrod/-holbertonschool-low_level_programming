2e32fea03734834e60e0a0dcb4c5c7ccdc2f865f
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-02-23 16:46:01 -0400

    making the absss or the 6 jajaj

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - making something with absolute value
+ *
+ *@num: la variable
+ * Return: 0
+**/
+int _abs(int num)
+{
+if (num < 0)
+{
+	_putchar(num = -num);
+}
+
+return (0);
+}
