3ff3e956d1b3a32c228901d0e7d672d8504f1884
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-02-28 14:55:40 -0400

    fixing betty

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,14 @@
 */
 void print_rev(char *s)
 {
-	int i =0, index, len;
+	int i = 0, index, len;
 	while (s[i] != '\0')
 	{
 		i++;
-	}
-	
+	}	
 	len = i;
-	for (index = len - 1 index >= 0; index--)
+
+	for (index = len - 1; index >= 0; index--)
 	{
 		_putchar(s[index]);
 	}
