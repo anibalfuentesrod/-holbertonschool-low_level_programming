2a42481c2d4be5be7745f025f7cefa1ab3e39ce4
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-02-28 10:53:58 -0400

    making this with the help of yadiel

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen;
+ *
+ *@s: the variable
+ *
+ * Return:
+**/
+
+int _strlen(char *s)
+{
+int i;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+return(i);
+}
