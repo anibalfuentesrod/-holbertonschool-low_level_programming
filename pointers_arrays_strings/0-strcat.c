d9c82f5892f50d78aa614f049e14d7af81f429ae
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-01 12:41:52 -0400

    fixing the betty

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,17 @@
 char *_strcat(char *dest, char *src)
 {
 	char *ogdest = dest;
+
 	while (*dest != '\0')
 	{
 		dest++;
 	}
 	while (*src != '\0')
-		{
-			*dest = *src;
-			dest++;
-			src++;
-		}
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
 	*dest = '\0';
 	return (ogdest);
 }
