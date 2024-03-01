b394bb88b382e9212a1d2d250a41d4c2e5979d2c
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-01 19:18:06 -0400

    fixing betty for 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -5,19 +5,18 @@
  * @dest: the destinaiton
  * @src: where you whant to take the string
  * @n: the varibale
- * Return:
+ * Return: dest null
 **/
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	
 		for (i = 0; i < n; i++)
 		{
 			dest[i] = src[i];
 		}
 
 			for (; n > i; i++)
-			{	
+			{
 				dest[i] = '\0';
 			}
 return (dest);
