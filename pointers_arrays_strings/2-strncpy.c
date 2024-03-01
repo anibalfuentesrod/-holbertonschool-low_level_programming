70c6db396743a0727a91c32b38469be5cad50395
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-01 19:16:14 -0400

    making the 2 with chatgpt

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy - the name of function
+ *
+ * @dest: the destinaiton
+ * @src: where you whant to take the string
+ * @n: the varibale
+ * Return:
+**/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	
+		for (i = 0; i < n; i++)
+		{
+			dest[i] = src[i];
+		}
+
+			for (; n > i; i++)
+			{	
+				dest[i] = '\0';
+			}
+return (dest);
+}
