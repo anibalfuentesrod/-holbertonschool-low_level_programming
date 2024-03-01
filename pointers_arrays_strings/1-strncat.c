94ecb141d24ffbea301c2900c78fdc43ee9714ea
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-01 14:13:51 -0400

    making the 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strncat - the name of function
+ *
+ * @dest: variable name
+ * @src: varibale name
+ * @n: the num variable
+ * Return:
+**/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ogdes = dest;
+	size_t i = 0;
+	n = 0;
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while ((size_t)i < (size_t) n && *src != '\0')
+	{
+		*dest++ = *src++;
+
+	}
+	return (ogdes);
+}
