7304837973f52db71b85c51a0fd97ae029249023
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-05 19:21:29 -0400

    making the 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - the name of function
+ *
+ * @s: the string
+ * Return: s[i]
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+	}
+	return (s[i]);
+}
