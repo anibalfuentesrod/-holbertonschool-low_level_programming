29f8f68738fe2360afa459d6afdc428852fca658
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-05 10:45:03 -0400

    making the 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _strspn - the name of the three
+ *
+ * @s: the string to be copied
+ * @accept: ....i dont know to be honest
+ *
+ * Return: s and accept
+**/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	unsigned int c = 0;
+	const char *pp;
+
+	while (*s)
+	{
+		i = 0;
+		pp = accept;
+		
+		while (*pp)
+		{
+			if (*pp == *s)
+			{
+				i = 1;
+				break;
+			}
+			pp++;
+		}
+		if (!i)
+			break;
+		c++;
+		s++;
+	}
+	return (c);
+}
