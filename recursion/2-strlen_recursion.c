d9b6eac9cb5f9b8500fac11bc89056117f0eb306
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-05 19:30:15 -0400

    ajajajjajaj fixing it aloneeeee jajajaajaja

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,10 +8,11 @@
 
 int _strlen_recursion(char *s)
 {
-	int i = 0;
 
-	if (s[i] != '\0')
+	if (*s == '\0')
 	{
+		return (0);
 	}
-	return (s[i]);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
