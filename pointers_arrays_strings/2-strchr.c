de9c3febbbe572ff2cabba284aa6f2d3e02d064a
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-05 09:15:48 -0400

    making the 2 with the help of chatgpt

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - the namo of prototype
+ *
+ * @s: the name of varibale
+ * @c: the namo of second varibale
+ *
+ * Return: c in the stirng s, or NULL
+**/
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
