5de33c71430e410d576a6ca19d7c7f27bd56bd04
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-08 10:51:42 -0400

    making the 1 and betty

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - the namo of function
+ *
+ * @argc: argument count
+ * Return: 0
+**/
+int main(int argc)
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
