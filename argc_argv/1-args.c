5353594ecb36b457f24ce350c2bf66fa718af555
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-08 12:38:54 -0400

    making the 1 trying to fix betty

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,14 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include"main.h"
+int main(int argc, char *argv[]);
 /**
  * main - the namo of function
  *
  * @argc: argument count
+ * @argv: the argument vector
  * Return: 0
 **/
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc);
+	(void)argv;
+
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
