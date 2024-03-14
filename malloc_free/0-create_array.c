0aa2a7373b63be4ee9efe0b36acf6ad1f546634c
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-14 10:37:14 -0400

    making the 0 right

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,6 +14,11 @@ char *create_array(unsigned int size, char c)
 
 	char *array = (char *)malloc(size * sizeof(char));
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
 	if (array == NULL)
 	{
 		return (NULL);
