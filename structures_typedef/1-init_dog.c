1ceef03c8d2def73e8b480dc91e1a4877bb5b6a6
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-14 14:29:44 -0400

    trying to fix betty

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 /**
  * init_dog - the name of function
+ * dog - the dog ajajaj
  * @d: the structure
  * @name:name of aaaaa i dont know bro
  * @age: age of...you know
  * @owner: the owner
- * struct dog - the structure of dog
 **/
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
