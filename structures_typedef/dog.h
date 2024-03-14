9b10e1d7033e2ebe1d9a5792a1ba8d5890fe646e
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-14 10:17:13 -0400

    making the 0 and betty too

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - the name of function
+ * @name: name of dog
+ * @age: the age of dog
+ * @owner: the owner obviooo cb
+ * Return:
+**/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
