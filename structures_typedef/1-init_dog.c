5093e477a635f0bf97368bc4917012575a7edaf7
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-14 14:11:51 -0400

    making the one with yahdiel

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * init_dog - the name of function
+ * @d: the structure
+ * @name:name of aaaaa i dont know bro
+ * @age: age of...you know
+ * @owner: the owner
+**/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+
+	d->age = age;
+
+	d->owner = owner;
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,18 +1,9 @@
-#ifndef DOG_H
-#define DOG_H
-#include <stdio.h>
-/**
- * struct dog - the name of function
- * @name: name of dog
- * @age: the age of dog
- * @owner: the owner obviooo cb
- * Return:
-**/
 struct dog
 {
 	char *name;
-	float age;
-	char *owner;
 
+	float age;
+
+	char *owner;
 };
-#endif
+void init_dog(struct dog *d, char *name, float age, char *owner);
