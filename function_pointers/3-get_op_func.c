30b5d0cd55d65acb7368d8ea72f8886e9545ff74
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-18 19:26:05 -0400

    trying to make the 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,35 +1,34 @@
-#include <stdio.h>
-#include "3-calc.h"
-#include <stddef.h>
 #include <stdlib.h>
-#include "function_pointers.h"
+#include "3-calc.h"
 
 /**
  * get_op_func - selects the corect function to perform the operator
  * @s: operator passed as argument to the program
  *
  * Return: ptr to the function that corresponds to the operator
- */
+*/
 
 int (*get_op_func(char *s))(int, int)
 {
+	int i;
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL}};
-	int i = 0;
+		{NULL, NULL}
+	};
+	i = 0;
 
-	while (ops[i].op != NULL)
+
+	while (i < 5)
 	{
-		if (*(ops[i].op) == *s && !(*(s + 1)))
+		if (*ops[i].op == *s && !(*(s + 1)))
 		{
 			return (ops[i].f);
 			i++;
 		}
 	}
-printf("Error\n");
-exit(99);
+return (NULL);
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,27 +12,34 @@
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-	int (*operator)(int, int);
+	int num1, num2;
 
-	if (argc != 4)
+	if ((argc) != 4)
 	{
 		printf("Error\n");
-		exit(90);
+
+		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	operator = get_op_func(argv[2]);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[3]);
 
-	if (operator == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+		if (get_op_func(argv[2]) == NULL)
+		{
+			printf("Error\n");
 
-	result = operator(num1, num2);
-	printf("%d\n", result);
+			exit(99);
+		}
 
-	return (0);
+		if (num2 == 0 && (*argv[2] == '%' || *argv[2] == '/'))
+		{
+			printf("Error\n");
+
+			exit(100);
+		}
+
+		printf("%d\n", (get_op_func(argv[2])(num1, num2)));
+
+		return (0);
+
 }
