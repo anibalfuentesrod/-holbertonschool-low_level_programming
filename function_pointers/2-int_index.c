7e04647c867d8b79d718f2f1c4e90019cc669c7a
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-18 10:54:34 -0400

    making the 2 and betty too

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - the name of function
+ * @array: the array
+ * @size: the size
+ * @cmp: I don't know to be honest
+ * Return: -1 if no element matches if size <= -1
+**/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
