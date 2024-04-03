539812301b0e170c5d4da65a708f16a81232fe87
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-04-03 15:08:53 -0400

    pushing the 3*

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -30,8 +30,12 @@ int copy_files(const char *src, const char *dest)
 	int fd_from, fd_to;
 	ssize_t bytes_read, bytes_written = 0;
 	char buffer[BUFFER_SIZE];
+	int i = 0;
 
-	if (strcmp(src,dest) == 0)
+	while (src[i] != '\0' && dest[i] != '\0' && src[i] == dest[i])
+		i++;
+
+	if (src[i] != '\0' && dest[i] == '\0')
 	{
 		dprintf(STDERR_FILENO, "Error: '%s' and '%s' are the same\n", src, dest);
 		return (1);
@@ -47,7 +51,10 @@ int copy_files(const char *src, const char *dest)
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
+		if (access(dest, W_OK) == -1){
+			dprintf(STDERR_FILENO, "Error: Permission enied to write to %s\n", dest);
+		} else
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
 		close(fd_from);
 		return (1);
 	}
@@ -72,7 +79,13 @@ int copy_files(const char *src, const char *dest)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		return (1);
-	} return (0);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		return (1);
+	}
+	return (0);
 }
 /**
  * main - the main duhh, and entry point of the program
